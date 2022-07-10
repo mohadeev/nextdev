0f7861ad4b49ba7fd891e3162594c9778ec46e8a
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-07-10 17:45:20 +0200

    SD

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,11 +1,19 @@
-import { news } from '../api/news';
+// import { news } from '../api/news';
 
-export default function Sulg(req, res) {
-  const newsArticle = news.filter((item) => item.slug === req.query.slug);
-  if (req.method === "GET") {
-    res.status(200).json(newsArticle);
-  } else {
-    res.setHeader("Allow", ["GET"]);
-    res.status(405).json({ message: `Method ${req.method} is not allowed` });
-  }
-}
+// export default function Sulg(req, res) {
+//   const newsArticle = news.filter((item) => item.slug === req.query.slug);
+//   if (req.method === "GET") {
+//     res.status(200).json(newsArticle);
+//   } else {
+//     res.setHeader("Allow", ["GET"]);
+//     res.status(405).json({ message: `Method ${req.method} is not allowed` });
+//   }
+// }
+
+import React from "react";
+
+const Slug = () => {
+  return <div>Slug</div>;
+};
+
+export default Slug;
